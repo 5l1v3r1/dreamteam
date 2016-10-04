9c91cabf7497f7e35e6fc090223d1606c3bede1a
Hoang Trung <hoangtrungweb@gmail.com>
2016-10-04 08:50:12 +0700

    Init

---
+++ inc/about.php
@@ -0,0 +1,7 @@
+<div id="aboutHolder">
+	<h1 id="headerAbout">About - Katarzyna Konkowska ---1---1</h1>
+	<div id="aboutName">KATARZYNA KONKOWSKA ---1---1</div>
+	<div id="aboutT1" class="wwwFont"> ---1---1 Katarzyna Konkowska is a stage and exhibition designer with many yearsâ experience in both television and the events industry. She has completed over 150 projects that range from large-scale exhibition stands to set designs for internationally recognized TV shows and prestigious events.<br><br>She holds an MA with distinction from the Academy of Fine Arts in Warsaw.Â&nbsp;Each of Katarzynaâs productions is an original design that she tailors to the particular event and venue, and each is built from scratch.</div>
+	<div id="aboutT2" class="wwwFont">In interpreting her clientâs brief Katarzyna abandons realism, drawing her inspiration from abstraction and futurism, and relying on her own vivid imagination. The possibilities of unbounded space and the concept of kinetic sculpture form the basis of her design philosophy.<br><br>She is fascinated by nature and light and often plays with gravity, making hefty constructions appear weightless. </div>
+	<div class="clear"></div>
+</div>
---
+++ inc/contact.php
@@ -0,0 +1,8 @@
+<div id="contactHolder">
+	<h1 id="headerContact">Contact - Katarzyna Konkowska</h1>
+	<div id="contactName">KATARZYNA KONKOWSKA</div>
+	<div id="contactT1"><span id="contactF1">M: </span><span id="contactF1_2">+48 506 029 729</span><div class="clear"></div></div>
+	<div id="contactT2"><div id="contactF2">E:&nbsp;</div><div id="contactF2_1"><div id="contactF2_2">studio@dreamteam.pl</div><div id="contactF2_3">studio@dreamteam.pl</div><div id="contactF2_4">studio@dreamteam.pl</div><div id="btnContact"></div></div><div class="clear"></div></div>
+	<div id="contactLine"></div>
+	<div class="clear"></div>
+</div>
